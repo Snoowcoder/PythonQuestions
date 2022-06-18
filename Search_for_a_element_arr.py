2182bf6e82512d6b0363b68029c8a76d0925e0b3
Aditya Jaiswal <97632498+Snoowcoder@users.noreply.github.com>
2022-06-18 23:39:18 +0530

    Add files via upload

---
+++ Search_for_a_element_arr.py
@@ -0,0 +1,21 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+arr = array('i', [])
+
+n = int(input("Enter the length of the Array "))
+
+for i in range(n):
+    x = int(input("Enter the Values you want in your Array "))
+    arr.append(x)
+    
+print(arr)
+
+
+result = int(input("Enter the value you want to search "))
+k = 0
+for j in arr:
+    if j == result:
+        print ("The Value is on " , k)
+        break
+    elif j != result:
+        print ("The Value is not Prsent")
